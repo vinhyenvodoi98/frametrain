3d14f7f32aa47fa793e846ee53931060c756d1c2
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2024-10-05 23:49:09 +0900

    feat: sync transaction

--- components/FrameEditor.tsx
+++ components/FrameEditor.tsx
@@ -22,7 +22,6 @@ import { Input } from './shadcn/Input'
 import { Popover, PopoverContent, PopoverTrigger } from './shadcn/Popover'
 import { Toggle } from './shadcn/Toggle'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './shadcn/Tooltip'
-import ConnectButton from './foundation/ConnectButton'
 
 export default function FrameEditor({
     frame,
@@ -211,8 +210,6 @@ export default function FrameEditor({
                     >
                         <PlayIcon className="w-4 h-4" />
                     </Toggle>
-
-                    <ConnectButton />
 
                     <Popover>
                         <PopoverTrigger asChild={true}>
